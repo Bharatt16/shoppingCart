f3f84a479949c08e2586ece90a64c256dd433dcf
Bharatt16 <194278228+Bharatt16@users.noreply.github.com>
2025-10-14 07:44:58 +0530

    completedd home.jsx and worked oon responsivness

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 const Navigation = () => {
   return (
-    <div className='flex items-center justify-between px-5 bg-red-400 h-20'>
+    <div className='text-white flex items-center justify-between px-5 h-20 absolute w-full'>
 
     
     <div className='flex items-center justify-between'>
         <img src="" alt="logo" />
-        <h1>Game Haven</h1>
+        <h1 className='sm:text-4xl font-medium '>Game Haven</h1>
     </div>
                                     
     <div>
         <label htmlFor="searchGames" className='flex items-center gap-2'>
-        <input type="search" name="searchGames" id="searchGames" placeholder='search games ..'   className="border-1 rounded-xl px-3 py-1 w-48 focus:w-72 hover:w-72 transition-all duration-500 outline-none"
+        <input type="search" name="searchGames" id="searchGames" placeholder='search games ..'   className="text-black bg-white border-1 px-3 py-1 w-48 sm:focus:w-72 sm:hover:w-72 transition-all duration-500 outline-none placeholder-gray-800"
         
         />
-        <div className='p-1 rounded-xl border-1 bg-white/20'>
-        <img className='h-5' src="/public/svgs/search.svg" alt="" />
+        <div className='sm:p-1 p-3 rounded-xl border-1  bg-black/20 '>
+        <img className='sm:h-5 ' src="/svgs/search.svg" style={{ filter: "brightness(0) invert(1)" }} alt="" />
 
         </div>
          </label>
@@ -24,10 +24,10 @@ const Navigation = () => {
 
     <div className='flex items-center justify-between'>
         <div className=''>
-        <img className='h-5 px-3 border-r-2 border-black' src="/public/svgs/add.svg" alt="addGames" />
+        <img className='h-5 px-3 border-r-2 border-black' src="/svgs/add.svg" style={{ filter: "brightness(0) invert(1)" }} alt="addGames" />
         </div>
         <div className=''>
-            <img className='h-5 px-3' src="/public/svgs/cart.svg" alt="cart" />
+            <img className='h-5 px-3' src="/svgs/cart.svg" style={{ filter: "brightness(0) invert(1)" }} alt="cart" />
         </div>
     </div>
     
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,33 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+    <div className='sm:pt-40 pt-30 sm:px-6 px-4 '>
        
-       <video src=""></video>
+       <video className='hidden sm:block fixed top-0 left-0 w-full h-full z-[-1] object-cover' src="/videos/desktopVideoBg.mp4" autoPlay loop muted playsInline></video>
+       <video className='block sm:hidden fixed top-0 left-0 w-full h-full z-[-1] object-cover' src="/videos/phoneVideoBg.mp4" autoPlay loop muted playsInline></video>
+       
+      <div className='flex w-full flex-col sm:flex-row gap-8'>
+          
+      <div className='sm:w-1/2 w-full flex flex-col gap-6 '>
+       <h1 className='text-white sm:text-6xl text-5xl'>Step into Game Haven, the digital home for gamers!</h1>
+          <p className='text-2xl text-white'>Shop top titles, explore new worlds, and level up your collection today.</p>
+          <button className='p-3 bg-white w-'>Explore</button>
+       </div>
 
+
+       <div className='sm:w-1/2 w-full text-white text-2xl flex flex-col gap-2 justify-between sm:items-end'>
+            <div className='p-5 h-full flex flex-col gap-5 justify-between backdrop-blur-xl shadow-2xl shadow-amber-400'>
+            <h1 className='text-4xl font-bold pb-4'>Quick NAvigation</h1>
+           <div className=' p-2 bg-white text-black' >Best of the year</div>
+           <div className=' p-2 bg-white text-black' >All time top</div>
+           <div className=' p-2 bg-white text-black' >Popular in 2025</div>
+           <div className=' p-2 bg-white text-black' >Last 30days</div>
+           <div className=' p-2 bg-white text-black' >Added</div>
+            </div>
+       </div>
+
+
+      </div>
     </div>
   )
 }
